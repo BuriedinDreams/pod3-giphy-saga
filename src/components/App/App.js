a616aec4a55e68e9fffd34e347a6d3ef4b0b8607
Vada Karlen <vada.karlen@gmail.com>
2021-03-04 13:24:28 -0600

    Set up framework

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+import Header from '../Header/Header';
+import SearchPage from '../SearchPage/SearchPage';
+import FavoritePage from '../FavoritePage/FavoritePage';
 
 function App(props) {
   return (
     <div>
-      <h1>Giphy Search!</h1>
+      <Router>
+        <Header />
+
+        <Route path="/" exact>
+          <SearchPage />
+        </Route>
+
+        <Route path="/favorites">
+          <FavoritePage />
+        </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div> Dont forget Nav</div>;
+}
+
+export default Header;
