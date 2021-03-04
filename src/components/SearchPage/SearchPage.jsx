c25f945a4655c93d7ac3000ef61a1c4becf9df7c
Zack Werner <allthelight044@gmail.com>
2021-03-04 13:49:08 -0600

    added basic framework

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,5 +1,17 @@
+import SearchBar from '../SearchBar/SearchBar'
+import SearchResults from '../SearchResults/SearchResults'
+
 function SearchPage() {
-  return <div>search page</div>;
+  return (
+  <div>
+    <input type="text"></input>
+    <button>Search</button>
+    <SearchResults/>
+  </div>
+  );
 }
 
 export default SearchPage;
+
+
+// This is going to 
