f3d7491bd0d750c76db449be9e844732b9af7252
patrickrnelson <patrickrnelson1@gmail.com>
2021-03-04 13:43:40 -0600

    index saga and store setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga() {
+  // Dispatch/put listeners
+  yield takeEvery('FETCH_PLANTS', fetchPlants)
+}
+
+// Redux store
+const storeInstance = createStore(
+  combineReducers({  }),
+  applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={storeInstance}>
+  <App />
+  </Provider>,
+  document.getElementById('root'));
+
